d4e00992c78433c56dbc233c1d03f9d6c3d8ced9
pablorod2809 <pablorodriguez@uxorit.com>
2024-08-22 08:53:48 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+async function fetchHTML() {
+    try {
+        const response = await fetch('https://redis.io/docs/latest/commands/geoadd/', {
+            mode: 'cors', // Asegúrate de que el servidor permita CORS
+        });
+        if (!response.ok) throw new Error('Network response was not ok');
+        const html = await response.text();
+        document.getElementById('content').innerHTML = html;
+    } catch (error) {
+        console.error('Error fetching HTML:', error);
+        document.getElementById('content').innerText = 'Error al obtener el contenido.';
+    }
+}
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.error('ServiceWorker registration failed:', error);
+            });
+    });
+}
+
+fetchHTML();
+
