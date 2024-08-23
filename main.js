f35ead564d837a32743be6767cb91ea8f4a25958
pablorod2809 <pablorodriguez@uxorit.com>
2024-08-23 17:18:42 -0300

    icono

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('js/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
                 console.log('ServiceWorker registered with scope:', registration.scope);
             })
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'nio-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/js/main.js',
+    '/main.js',
     '/manifest.json'
 ];
 
